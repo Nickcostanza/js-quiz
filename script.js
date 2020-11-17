67562a942be9a8f5331ad9ef7d83b7a344f05373
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-11-16 16:50:05 -0800

    script file almmost done, questions not rendering

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const nextBtn = document.getElementById('next-btn');
 const finishBtn = document.getElementById('finish-btn');
 const maincCardEl = document.getElementById('main-card');
 const questionCardEl = document.getElementById('question-card');
-const timer = document.getElementById('timer');
+const timerEL = document.getElementById('timer');
 const question = document.getElementById('question');
 const answerBtn = document.getElementById('answer-buttons');
 const points = document.getElementById('score-count')
 
 let scoreCount = 0;
 
-const getRandomQuestion, questionIndex
+var getRandomQuestion, questionIndex;
 
 startBtn.addEventListener('click', startGame);
 
@@ -24,10 +24,10 @@ finishBtn.addEventListener('click', endGame);
 
 function startGame() {
     maincCardEl.classList.add('hide')
-    getRandomQuestion = quesitons.sort(() => Math.random() - .5)
+    getRandomQuestion = questions.sort(() => Math.random() - .5)
     questionIndex = 0
     questionCardEl.classList.remove('hide')
-    timer.classList.remove('hide')
+    timerEL.classList.remove('hide')
     points.classList.remove('hide')
     nextQuestion()
     timer()
@@ -48,7 +48,7 @@ function resetState() {
 
 function showQuestion(question) {
     question.innerText = question.question;
-    quesiton.answers.forEach(answers => {
+    question.answers.forEach(answers => {
         const button = document.createElement('button')
         button.innerText = answers.text;
         button.classList.add('btn');
@@ -61,6 +61,163 @@ function showQuestion(question) {
     })
 }
 
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const correct = selectedBtn.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerBtn.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+
+    if (getRandomQuestion.length > questionIndex + 1) {
+        nextBtn.classList.remove('hide');
+        if (e.target.dataset.correct === 'true') {
+            scoreCount++;
+            document.getElementById('scoreDisplay').innerHTML = `Points: ${scoreCount}`
+        }
+    } else {
+        if (e.target.dataset.correct === 'true') {
+            scoreCount++;
+            points.classList.add('hide')
+            console.log(scoreCount)
+        }
+        localStorage.setItem('score', scoreCount);
+
+        finishBtn.classList.remove('hide');
+    }
+}
 
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
 
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+
+}
+
+function timer() {
+    let sec = 120;
+    let timer = setInterval(function () {
+        document.getElementById('timerDisplay').innerHTML = '' + sec + ' seconds remaining'
+        sec--
+        if (sec < 0) {
+            clearInterval(timer)
+            alert('Time has Run out')
+            endGame()
+        }
+     }, 1000)
+} 
+
+function endGame() {
+    window.location.href = 'scores.html'
+}
+
+
+var questions = [
+    {
+        question: 'How many Super Bowl rings does Tom Brady have?',
+        answers: [
+            { text:'2', correct: false},
+            { text: '4', correct: false},
+            { text: '6', correct: true},
+            { text: '5', correct: false}
+        ]
+    },
+
+    {
+        question: 'What is Philadelphias Mascot?',
+        answers: [
+            { text: 'Giants', correct: false},
+            { text: 'Eagles', correct: true},
+            { text: 'Falcons', correct: false},
+            { text: 'Flyers', correct: false}
+        ]
+    },
+
+    {
+        question: 'Who won the First ever Superbowl?',
+        answers: [
+            { text: 'Chiefs' , correct: false },
+            { text: 'Browns', correct: false },
+            { text: 'Bears', correct: false },
+            { text: 'Packers', correct: true }
+        ]
+    },
+
+    {
+        question: 'Who one MVP for the 2018 regular season?',
+        answers: [
+            { text: 'Patrick Mahomes' , correct: true },
+            { text: 'Lamar Jackson' , correct: false },
+            { text: 'Tom Brady', correct: false },
+            { text: 'Russell Wilson', correct: false } 
+        ]
+    },
+
+    {
+        question: 'What year did the NFL Start?',
+        answers: [
+            { text: '1920' , correct: true },
+            { text: '1938', correct: false },
+            { text: '1907', correct: false },
+            { text: '1960', correct: false }
+        ]
+    },
+
+    {
+        question: 'How many superbowls have the Seattle Seahawks won?',
+        answers: [
+            { text: '4' , correct: false },
+            { text: '0' , correct: false },
+            { text: '1', correct: true },
+            { text: '2' , correct: false } 
+        ]
+    },
+
+    {
+        question: 'Which NFL team plays at Mile High?',
+        answers: [
+            { text: 'Falcons' , correct: false },
+            { text: 'Ravens', correct: false },
+            { text: 'Titans', correct: false },
+            { text: 'Broncos', correct: true }  
+        ]
+    },
+    {
+        question: 'Who Led the NFL in Rushing Yards in 2019?',
+        answers: [
+            { text: 'Derek Henry' , correct: true },
+            { text: 'Nick Chubb', correct: false },
+            { text: 'Todd Gurley', correct: false },
+            { text: 'Alvin Kamara', correct: false } 
+        ]
+    },
+
+    {
+        question: 'Which of the following players plays Wide Receiver?',
+        answers: [
+            { text: 'Calvin Johnson' , correct: true },
+            { text: 'Deshaun Watson', correct: false },
+            { text: 'Brian Dawkins', correct: false },
+            { text: 'Jalen Ramsey' , correct: false } 
+        ]
+    },
+
+    {
+        question: 'Whos the greats NFL team?',
+        answers: [
+            { text: 'not the Patriots' , correct: false },
+            { text: 'definitely not the cowboys', correct: false},
+            { text: 'Packers? noooo', correct: false },
+            { text: 'Eagles!! GO BIRDS', correct: true }  
+        ]
+    },
+]
